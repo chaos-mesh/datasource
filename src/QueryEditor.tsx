398a2b94bae7fc088f35f8f0343adaa9dff80348
yeya24 <yb532204897@gmail.com>
2020-06-21 12:02:05 -0400

    restructure query editor
    Signed-off-by: yeya24 <yb532204897@gmail.com>

--- src/QueryEditor.tsx
+++ src/QueryEditor.tsx
@@ -1,63 +1,95 @@
 import defaults from 'lodash/defaults';
 
-import React, { ChangeEvent, PureComponent } from 'react';
-import { InlineFormLabel, LegacyForms } from '@grafana/ui';
-import { QueryEditorProps, SelectableValue } from '@grafana/data';
-import { DataSource } from './DataSource';
-import { defaultQuery, MyDataSourceOptions, EventsQuery } from './types';
+import React, {ChangeEvent, PureComponent} from 'react';
+import {InlineFormLabel, LegacyForms} from '@grafana/ui';
+import {QueryEditorProps, SelectableValue} from '@grafana/data';
+import {DataSource} from './DataSource';
+import {defaultQuery, MyDataSourceOptions, EventsQuery} from './types';
 
-const { FormField, Select } = LegacyForms;
+const {Input, Select} = LegacyForms;
 
 type Props = QueryEditorProps<DataSource, EventsQuery, MyDataSourceOptions>;
 
 const kindOptions: Array<SelectableValue<string>> = [
-  { value: 'PodChaos', label: 'pod chaos' },
-  { value: 'NetworkChaos', label: 'network chaos' },
-  { value: 'IOChaos', label: 'io chaos' },
-  { value: 'TimeChaos', label: 'time chaos' },
-  { value: 'KernelChaos', label: 'kernel chaos' },
-  { value: 'StressChaos', label: 'stress chaos' },
+  {value: 'PodChaos', label: 'pod chaos'},
+  {value: 'NetworkChaos', label: 'network chaos'},
+  {value: 'IOChaos', label: 'io chaos'},
+  {value: 'TimeChaos', label: 'time chaos'},
+  {value: 'KernelChaos', label: 'kernel chaos'},
+  {value: 'StressChaos', label: 'stress chaos'},
 ];
 
 export class QueryEditor extends PureComponent<Props> {
+  query: EventsQuery;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.query = defaults(this.props.query, defaultQuery);
+  }
+
   onNamespaceChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onChange, query, onRunQuery } = this.props;
-    onChange({ ...query, namespace: event.target.value });
-    onRunQuery();
+    this.query.namespace = event.target.value;
   };
 
   onKindChange = (item: SelectableValue<string>) => {
-    const { onChange, query, onRunQuery } = this.props;
-    onChange({ ...query, kind: item.value! });
-    onRunQuery();
+    this.query.kind = item.value!;
   };
 
   onExperimentChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onChange, query, onRunQuery } = this.props;
-    onChange({ ...query, experiment: event.target.value });
-    onRunQuery();
+    this.query.experiment = event.target.value;
+  };
+
+  onRunQuery = () => {
+    const {query} = this;
+    this.props.onChange(query);
+    this.props.onRunQuery();
   };
 
   render() {
-    const query = defaults(this.props.query, defaultQuery);
-    const { namespace, experiment } = query;
-    const selectedKind = kindOptions.find(o => o.value === query.kind);
+    const {namespace, experiment} = this.query;
+    const selectedKind = kindOptions.find(o => o.value === this.query.kind);
 
     return (
       <div>
-        <div className="gf-form">
-          <FormField width={4} value={namespace || ''} onChange={this.onNamespaceChange} label="Namespace" />
-          <FormField
-            labelWidth={12}
-            value={experiment || ''}
-            onChange={this.onExperimentChange}
-            label="Experiment Name"
-          />
-        </div>
+        <div className="gf-form-inline">
+          <div className="gf-form">
+            <InlineFormLabel width={12} tooltip="Namespace of Chaos Experiments">
+              Experiment Namespace
+            </InlineFormLabel>
+            <Input
+              type="text"
+              className="gf-form-input"
+              value={namespace}
+              onChange={this.onNamespaceChange}
+              onBlur={this.onRunQuery}
+            />
+          </div>
 
-        <div className="gf-form">
-          <InlineFormLabel width={8}>Chaos Kind</InlineFormLabel>
-          <Select width={8} value={selectedKind} onChange={this.onKindChange} options={kindOptions} />
+          <div className="gf-form">
+            <InlineFormLabel width={12} tooltip="Name of the Chaos Experiment">
+              Experiment Name
+            </InlineFormLabel>
+            <Input
+              type="text"
+              className="gf-form-input"
+              value={experiment}
+              onChange={this.onExperimentChange}
+              onBlur={this.onRunQuery}
+            />
+          </div>
+
+          <div className="gf-form">
+            <InlineFormLabel width={8}>Chaos Kind</InlineFormLabel>
+            <Select
+              width={8}
+              value={selectedKind}
+              onChange={this.onKindChange}
+              onBlur={this.onRunQuery}
+              options={kindOptions}
+              isSearchable={false}
+            />
+          </div>
         </div>
       </div>
     );
