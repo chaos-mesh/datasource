4a0cb43e4114e0b708b4c94acabfc26b377e4d5f
Ben Ye <yb532204897@gmail.com>
2020-07-13 09:14:46 -0400

    use new dry events API
    Signed-off-by: Ben Ye <yb532204897@gmail.com>

--- src/types.ts
+++ src/types.ts
@@ -28,18 +28,6 @@ export interface ChaosEvent {
   Message: string;
   StartTime: string;
   FinishTime: string;
-  Pods: Array<{
-    ID: string;
-    CreateAt: string;
-    UpdateAt: string;
-    DeleteAt: string | null;
-    EventID: number;
-    PodIP: string;
-    PodName: string;
-    Namespace: string;
-    Message: string;
-    Action: string;
-  }>;
 }
 
 export interface ChaosEventsQueryResponse {
