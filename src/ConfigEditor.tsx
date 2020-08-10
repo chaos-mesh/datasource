3a0038e80ffb34702d47922a59162244da182c0b
Ben Ye <yb532204897@gmail.com>
2020-08-09 21:52:06 -0400

    support limit parameter in events query
    Signed-off-by: Ben Ye <yb532204897@gmail.com>

--- src/ConfigEditor.tsx
+++ src/ConfigEditor.tsx
@@ -27,22 +27,52 @@ export class ConfigEditor extends PureComponent<Props, State> {
     });
   };
 
+  onLimitChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      jsonData: {
+        ...options.jsonData,
+        limit: parseInt(event.target.value, 10),
+      },
+    });
+  };
+
   render() {
     const { options } = this.props;
-    const { url } = options;
+    const { url, jsonData } = options;
+    const { limit } = jsonData;
 
     return (
       <div className="gf-form-group">
-        <h3 className="page-heading">HTTP</h3>
-        <div className="gf-form">
-          <FormField
-            label="URL"
-            labelWidth={11}
-            onChange={this.onURLChange}
-            value={url || ''}
-            tooltip="Specify a complete HTTP URL (for example http://your_server:2333);"
-            placeholder="http://localhost:2333"
-          />
+        <div className="gf-form-group">
+          <h3 className="page-heading">HTTP</h3>
+          <div className="gf-form">
+            <FormField
+              label="URL"
+              labelWidth={10}
+              inputWidth={13}
+              onChange={this.onURLChange}
+              value={url || ''}
+              tooltip="Specify a complete HTTP URL (for example http://your_server:2333);"
+              placeholder="http://localhost:2333"
+            />
+          </div>
+        </div>
+
+        <div className="gf-form-group">
+          <h3 className="page-heading">Options</h3>
+          <div className="gf-form">
+            <FormField
+              label="Limit"
+              labelWidth={10}
+              inputWidth={6}
+              onChange={this.onLimitChange}
+              value={limit || ''}
+              tooltip="Limit the number of returned Chaos Events. Default value is 25. If you want to display more events, please increase the limit."
+              placeholder="25"
+            />
+          </div>
         </div>
       </div>
     );
