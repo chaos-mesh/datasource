0df855047b8dc268f24c0689872ae753e2d90c58
Yue Yang <g1enyy0ung@gmail.com>
2022-06-08 16:34:31 +0800

    chore: update sort imports
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   ...require('./node_modules/@grafana/toolkit/src/config/prettier.plugin.config.json'),
   arrowParens: 'avoid',
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    '^@ui/(.*)$',
+    'store',
+    '^slices/(.*)$',
+    '^components/(.*)$',
+    '^lib/(.*)$',
+    '^images/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
 };
