c7192de2bd5fd461ab6a8b7a693623d4af71d0fe
Yue Yang <g1enyy0ung@gmail.com>
2022-08-11 11:13:10 +0800

    chore: update bundle dir
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- bundle.sh
+++ bundle.sh
@@ -2,6 +2,7 @@ VERSION=$1
 ID=chaosmeshorg-datasource
 
 rm -rf dist
+rm -rf $ID
 
 echo "Bundled Version: $VERSION"
 echo "Start to build..."
@@ -12,7 +13,8 @@ yarn sign
 
 echo "Bundling..."
 
-zip -r $ID-$VERSION.zip dist -x "*.DS_Store*"
+mkdir $ID && cp -R dist/* $ID
+zip -r $ID-$VERSION.zip $ID -x "*.DS_Store*"
 md5sum $ID-$VERSION.zip > $ID-$VERSION.zip.md5
 
 echo "Done."
